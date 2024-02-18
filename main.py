5383115f5112a6cf3e7a548006eb11c8b798adaf
Basil <92749602+basil-dsouza@users.noreply.github.com>
2024-02-18 23:08:46 +0000

    Initial Basic no Frills implementation

--- main.py
+++ main.py
@@ -1,3 +1,83 @@
+direction = ""
+prev_direction = ""
+start_time = 0
+speed_fast = 255
+speed_slow = 100
+speed_turn_offset = 50
+speed = 0
+line_follower = False
+serial.redirect_to_usb()
+
 def on_forever():
-    pass
+    global start_time, prev_direction
+    start_time = control.millis()
+    if prev_direction != direction:
+        prev_direction = direction
+        serial.write_line("Direction:" + direction)
+        if direction == "FORWARD":
+            motor.motor_run(motor.Motors.M1, motor.Dir.CCW, speed)
+            motor.motor_run(motor.Motors.M2, motor.Dir.CCW, speed)
+            motor.motor_run(motor.Motors.M3, motor.Dir.CCW, speed)
+            motor.motor_run(motor.Motors.M4, motor.Dir.CCW, speed)
+        elif direction == "REVERSE":
+            motor.motor_run(motor.Motors.M1, motor.Dir.CW, speed)
+            motor.motor_run(motor.Motors.M2, motor.Dir.CW, speed)
+            motor.motor_run(motor.Motors.M3, motor.Dir.CW, speed)
+            motor.motor_run(motor.Motors.M4, motor.Dir.CW, speed)
+        elif direction == "ROTATE_CLOCKWISE":
+            motor.motor_run(motor.Motors.M1,
+                motor.Dir.CCW,
+                min(255, speed + speed_turn_offset))
+            motor.motor_run(motor.Motors.M2,
+                motor.Dir.CCW,
+                min(255, speed + speed_turn_offset))
+            motor.motor_run(motor.Motors.M3,
+                motor.Dir.CW,
+                min(255, speed + speed_turn_offset))
+            motor.motor_run(motor.Motors.M4,
+                motor.Dir.CW,
+                min(255, speed + speed_turn_offset))
+        elif direction == "ROTATE_COUNTERCLOCKWISE":
+            motor.motor_run(motor.Motors.M1,
+                motor.Dir.CW,
+                min(255, speed + speed_turn_offset))
+            motor.motor_run(motor.Motors.M2,
+                motor.Dir.CW,
+                min(255, speed + speed_turn_offset))
+            motor.motor_run(motor.Motors.M3,
+                motor.Dir.CCW,
+                min(255, speed + speed_turn_offset))
+            motor.motor_run(motor.Motors.M4,
+                motor.Dir.CCW,
+                min(255, speed + speed_turn_offset))
+        elif direction == "TRAVERSE_LEFT":
+            motor.motor_run(motor.Motors.M1, motor.Dir.CCW, speed)
+            motor.motor_run(motor.Motors.M2, motor.Dir.CW, speed)
+            motor.motor_run(motor.Motors.M3, motor.Dir.CW, speed)
+            motor.motor_run(motor.Motors.M4, motor.Dir.CCW, speed)
+        elif direction == "TRAVERSE_RIGHT":
+            motor.motor_run(motor.Motors.M1, motor.Dir.CW, speed)
+            motor.motor_run(motor.Motors.M2, motor.Dir.CCW, speed)
+            motor.motor_run(motor.Motors.M3, motor.Dir.CCW, speed)
+            motor.motor_run(motor.Motors.M4, motor.Dir.CW, speed)
+        elif direction == "STOP":
+            motor.motor_stop_all()
+        elif direction == "\"HORN\"":
+            music.play(music.tone_playable(262, music.beat(BeatFraction.BREVE)),
+                music.PlaybackMode.IN_BACKGROUND)
+        else:
+            motor.motor_stop_all()
 basic.forever(on_forever)
+
+def on_forever2():
+    global direction
+    if line_follower:
+        if pins.digital_read_pin(DigitalPin.P1) == 1:
+            direction = "FORWARD"
+        elif pins.digital_read_pin(DigitalPin.P2) == 1:
+            direction = "ROTATE_CLOCKWISE"
+        elif pins.digital_read_pin(DigitalPin.P0) == 1:
+            direction = "ROTATE_COUNTERCLOCKWISE"
+        else:
+            direction = "STOP"
+basic.forever(on_forever2)
--- main.ts
+++ main.ts
@@ -1,3 +1,66 @@
-basic.forever(function on_forever() {
-    
+let direction = ""
+let prev_direction = ""
+let start_time = 0
+let speed_fast = 255
+let speed_slow = 100
+let speed_turn_offset = 50
+serial.redirectToUSB()
+basic.forever(function () {
+    start_time = control.millis()
+    if (prev_direction != direction) {
+        let speed = 0
+        prev_direction = direction
+        serial.writeLine("Direction:" + direction)
+        if (direction == "FORWARD") {
+            motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, speed)
+            motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, speed)
+            motor.MotorRun(motor.Motors.M3, motor.Dir.CCW, speed)
+            motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, speed)
+        } else if (direction == "REVERSE") {
+            motor.MotorRun(motor.Motors.M1, motor.Dir.CW, speed)
+            motor.MotorRun(motor.Motors.M2, motor.Dir.CW, speed)
+            motor.MotorRun(motor.Motors.M3, motor.Dir.CW, speed)
+            motor.MotorRun(motor.Motors.M4, motor.Dir.CW, speed)
+        } else if (direction == "ROTATE_CLOCKWISE") {
+            motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, Math.min(255, speed + speed_turn_offset))
+            motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, Math.min(255, speed + speed_turn_offset))
+            motor.MotorRun(motor.Motors.M3, motor.Dir.CW, Math.min(255, speed + speed_turn_offset))
+            motor.MotorRun(motor.Motors.M4, motor.Dir.CW, Math.min(255, speed + speed_turn_offset))
+        } else if (direction == "ROTATE_COUNTERCLOCKWISE") {
+            motor.MotorRun(motor.Motors.M1, motor.Dir.CW, Math.min(255, speed + speed_turn_offset))
+            motor.MotorRun(motor.Motors.M2, motor.Dir.CW, Math.min(255, speed + speed_turn_offset))
+            motor.MotorRun(motor.Motors.M3, motor.Dir.CCW, Math.min(255, speed + speed_turn_offset))
+            motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, Math.min(255, speed + speed_turn_offset))
+        } else if (direction == "TRAVERSE_LEFT") {
+            motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, speed)
+            motor.MotorRun(motor.Motors.M2, motor.Dir.CW, speed)
+            motor.MotorRun(motor.Motors.M3, motor.Dir.CW, speed)
+            motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, speed)
+        } else if (direction == "TRAVERSE_RIGHT") {
+            motor.MotorRun(motor.Motors.M1, motor.Dir.CW, speed)
+            motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, speed)
+            motor.MotorRun(motor.Motors.M3, motor.Dir.CCW, speed)
+            motor.MotorRun(motor.Motors.M4, motor.Dir.CW, speed)
+        } else if (direction == "STOP") {
+            motor.motorStopAll()
+        } else if (direction == "\"HORN\"") {
+            music.play(music.tonePlayable(262, music.beat(BeatFraction.Breve)), music.PlaybackMode.InBackground)
+        } else {
+            motor.motorStopAll()
+        }
+    }
+})
+basic.forever(function () {
+    let line_follower = 0
+    if (line_follower) {
+        if (pins.digitalReadPin(DigitalPin.P1) == 1) {
+            direction = "FORWARD"
+        } else if (pins.digitalReadPin(DigitalPin.P2) == 1) {
+            direction = "ROTATE_CLOCKWISE"
+        } else if (pins.digitalReadPin(DigitalPin.P0) == 1) {
+            direction = "ROTATE_COUNTERCLOCKWISE"
+        } else {
+            direction = "STOP"
+        }
+    }
 })
